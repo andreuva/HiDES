added colorbar to plots, removed class status (not feaseble), added new initial conditions (second mode) and restructurate boundary conditions. NUMERICAL INSTABILITY NOT FIXED

PYTHON/MAIN.py
@@ -15,12 +15,10 @@ import conversions as convers
 from boundary_conditions import boundc
 from advance import advance
 import representation as rp
-import state_class as sc
 # --------------------------------------------------------------------
 # 			read the initial parameters of the simulation
 # --------------------------------------------------------------------
 import parameters as param
-
 # --------------------------------------------------------------------
 # 			Check inconsintency in the initial parameters
 # --------------------------------------------------------------------
@@ -39,9 +37,10 @@ if ((param.nintx <= 1) or (param.nintz <= 1)):
 
 if ((param.ondx > param.nintx) or (param.ondz > param.nintz)):
 	print( 'WARNING: The Number of wave N (ond(x,z))) in k = 2pi*ond(x,z)*/L should be Nint(x,z) or smaller' )
-	print( 'ondx = ',param.ondx,'	nintx = ',param.nintx)   ;   print( 'Nintz = ',param.ondz,'  nintz = ',param.nintz)
+	print( 'ondx = ',param.ondx,'	nintx = ',param.nintx)   ;   print( 'ondz = ',param.ondz,'  nintz = ',param.nintz)
 	print( 'ondx and ondz has been redefined as nintx and nintz.' )
-	ondx = nintx ; ondz = nintz 
+	param.ondx = param.nintx ; param.ondz = param.nintz 
+	print( 'ondx = ',param.ondx,'	nintx = ',param.nintx)   ;   print( 'ondz = ',param.ondz,'  nintz = ',param.nintz)
 
 #check the boundary conditions and puts the 'periodic' as default
 if ((param.boundcond != 'periodic') and (param.boundcond != '0deriv')):
@@ -69,10 +68,9 @@ grid = g.grid(param)
 # --------------------------------------------------------------
 #the routine initrout sets up the initial condition.
 init_cond = ic.introut(param,grid)   
-
 # plot and store the initial conditions
 init_cond.plot_ic(param,grid, save=True)
-init_cond.save_ic()
+# init_cond.save_ic()
 # ------------------------------------------------------------
 # 			declare and/or initialize variables
 # ------------------------------------------------------------
@@ -86,44 +84,36 @@ Um    	= init_cond.Um
 Ue   	= init_cond.pres/(param.gamm-1.) + init_cond.Um*(init_cond.vx**2. + init_cond.vz**2.)/2.
 Upx  	= init_cond.vx*init_cond.Um
 Upz  	= init_cond.vz*init_cond.Um
-# the 'densities', in the sense of the conservation laws, 
-#      are 'Um', 'Upx', 'Upz' and  'Ue'. 
-
-state = sc.state(param, init_cond)
-
+# the 'densities', in the sense of the conservation laws,
+#      are 'Um', 'Upx', 'Upz' and  'Ue'.
 
 itt  = 0
 time = 0.
+
 # ------------------------------------------------------------
 # 						MAIN LOOP
 # ------------------------------------------------------------
 while (itt < param.itmax and time < param.timef):
 
-#	CALCULATE FLUXES FROM DENSITIES ('fluxes' is the name of the subroutine) 
+#	CALCULATE FLUXES FROM DENSITIES ('fluxes' is the name of the subroutine)
 	fmx,fmz, fpxx,fpxz,fpzz,fpzx, fex,fez = convers.dens_to_fluxes(Um,Upx,Upz,Ue)
-	state = convers.dens_to_fluxes(Um,Upx,Upz,Ue)
 
-#	TIMESTEP 
+#	TIMESTEP
 	dt=tstep(Um,Upx,Upz,cs, param, grid)
-	print(time)
+	print('time = ',np.round(time,3) ,'\t itterations = ',itt,'\t dt = ',dt)
 	if ((time+dt) > param.timef):
 		dt = param.timef-time
 
 #	ADVANCE ONE TIMESTEP using the chosen numerical scheme 
 	Umn,Upxn,Upzn,Uen = advance(dt, grid, Um,Upx,Upz,Ue,fmx,fmz,fpxx,fpxz,fpzz,fpzx,fex,fez)
 
-#	BOUNDARY CONDITIONS 
-	Umn = boundc('left',Umn);	Umn = boundc('right',Umn);
-	Uen = boundc('left',Uen);	Uen = boundc('right',Uen);
-	Umn = boundc('up',Umn);		Umn = boundc('down',Umn);
-	Uen = boundc('up',Uen);		Uen = boundc('down',Uen);
+#	BOUNDARY CONDITIONS
+	Umn = boundc(Umn)
+	Uen = boundc(Uen)
+	Upxn = boundc(Upxn)
+	Upzn = boundc(Upzn)
 
-	Upxn = boundc('left',Upxn);	Upxn = boundc('right',Upxn);
-	Upzn = boundc('left',Upzn);	Upzn = boundc('right',Upzn);
-	Upxn = boundc('up',Upxn);	Upxn = boundc('down',Upxn);
-	Upzn = boundc('up',Upzn);	Upzn = boundc('down',Upzn);     
-
-#	CALCULATE PRIMITIVE VARIABLES FROM THE DENSITIES    
+#	CALCULATE PRIMITIVE VARIABLES FROM THE DENSITIES
 	vxn,vzn,presn = convers.dens_to_state( Umn,Upxn,Upzn,Uen)
 
 #	EXCHANGE NEW AND OLD VARIABLES
@@ -132,35 +122,18 @@ while (itt < param.itmax and time < param.timef):
 
 	itt += 1      # advance itterations
 	time += dt # advance time
-    
-#     evolve the central point of the packet to track it
-#     if itype eq 'packet' then begin
-#       indz = (zc-z0)/dz
-#       zc = zc + interpolate(cs,npx/2,indz)*dt
-#     endif
-    
-#   PLOT RESULTS 
+
+#   PLOT RESULTS
 	if (itt%param.plt_cad == 0 or itt == 0 or time >= param.timef) :      
 
-		# if plttype == 'cutz':
-		#   vv = np.sqrt(vx*vx + vz*vz)
-		#   rp.drawing,Um[fix(npx/2),*],pres[fix(npx/2),*],vv[fix(npx/2),*], zz, time, itt
-		# elif plttype == 'cutx':
-		#   vv = sqrt(vx*vx + vz*vz)
-		#   drawing,Um[*,fix(npz/2)],pres[*,fix(npz/2)],vv[*,fix(npz/2)], xx, time, itt
-		# else :
-		rp.drawing_2d(param, Um,pres,vx,vz, time, itt, dt)
-    
-# #   STORE RESULTS - to store results in a file from time to time
-#     if itt mod store_cad eq 0 then begin
-#       save,/VARIABLES, FILENAME = 'results/states/estate_time_' $
-#         + strtrim(string(time,form='(f7.2)'),2)+'.sav'
-#       WRITE_PNG, 'results/plots/plot_time'+strtrim(string(itt+1d4,form='(f7.1)'),2)+'.png', TVRD(/TRUE)
-#     endif
-    
-#     #make rain if the mode is selected:)
-#     rain, rain_flag, rain_cad, itt, Um,pres,vx,vz   ,Upx,Upz,Ue,cs
-
+		if param.plt_type == 'cutz':
+			vv = np.sqrt(vx*vx + vz*vz)
+			# rp.drawing(Um[int(npx/2),:],pres[int(npx/2),:],vv[int(npx/2),:],grid.xx)
+		elif param.plt_type == 'cutx':
+			vv = sqrt(vx*vx + vz*vz)
+			# rp.drawing(Um[:,int(npz/2)],pres[:,int(npz/2)],vv[:,fix(npz/2)],grid.zz)
+		else :
+			rp.drawing_2d(init_cond, param, Um,pres,vx,vz, time, itt, dt)
 # ------------------------------------------------------------
 # 		END OF BIG LOOP -- RESULTS AND LAST OUTPUTS
 # ------------------------------------------------------------

PYTHON/introut.py
@@ -44,25 +44,35 @@ class introut():
 			# if its a sound wave compute the initial conditions with a cosine
 			# or if it's the second mode, a cosine + a phase in the velocity
 			if (p.shape == 'cosine'):
-
 				hz 		= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0))
-				hz_vx 	= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0))*np.cos(np.arctan(p.ondz/p.ondx))
-				hz_vz 	= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0))*np.sin(np.arctan(p.ondz/p.ondx))
-				
-				# compute the densities and pressures with the vale of the equilibrium + the perturbation
-				self.Um   = p.Um00  + p.Um00       *p.ampl*hz
-				self.pres = p.p00   + p.gamm*p.p00 *p.ampl*hz
-				
-				# if the wave is in one axis make sure we dont have the other axis perturbation
-				if (p.ondz == 0):
-					self.vx   = self.v00 + self.cs00*p.ampl*hz_vx
-					self.vz   = np.zeros_like(hz_vz)
-				elif (p.ondx == 0):
-					self.vx   = np.zeros_like(hz_vz)
-					self.vz   = self.v00 + self.cs00*p.ampl*hz_vz
-				else:
-					self.vx   = self.v00 + self.cs00*p.ampl*hz_vx
-					self.vz   = self.v00 + self.cs00*p.ampl*hz_vz
+				hz_vx 	= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0))*np.cos(np.arctan2(p.ondz,p.ondx))
+				hz_vz 	= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0))*np.sin(np.arctan2(p.ondz,p.ondx))
+			elif (p.shape == 'second mode'):
+				hz 		= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0))
+				hz_vx 	= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0) + np.pi)*np.cos(np.arctan2(p.ondz,p.ondx))
+				hz_vz 	= np.cos(2.*np.pi*p.ondx*g.gridx/(p.xf-p.x0) + 2.*np.pi*p.ondz*g.gridz/(p.zf-p.z0) + np.pi)*np.sin(np.arctan2(p.ondz,p.ondx))
+			else:
+				print('ERROR : there is no type '+p.shape+' for initial conditions type '+p.itype)
+				exit()
+
+			# compute the densities and pressures with the vale of the equilibrium + the perturbation
+			self.Um   = p.Um00  + p.Um00       *p.ampl*hz
+			self.pres = p.p00   + p.gamm*p.p00 *p.ampl*hz
+			
+			# if the wave is in one axis make sure we dont have the other axis perturbation
+			if (p.ondz == 0):
+				self.vx   = self.v00 + self.cs00*p.ampl*hz_vx
+				self.vz   = np.zeros_like(hz_vz)
+			elif (p.ondx == 0):
+				self.vx   = np.zeros_like(hz_vz)
+				self.vz   = self.v00 + self.cs00*p.ampl*hz_vz
+			else:
+				self.vx   = self.v00 + self.cs00*p.ampl*hz_vx
+				self.vz   = self.v00 + self.cs00*p.ampl*hz_vz
+
+		else:
+			print('ERROR : No initial conditions named '+p.itype)
+			exit()
 
 		# this calculates the sound speed array for the initial condition:
 		self.cs = np.sqrt(p.gamm*self.pres/self.Um)
@@ -71,18 +81,23 @@ class introut():
 
 		fig, axs = plt.subplots(2, 2, sharex='col', sharey='row')
 		(ax1, ax2), (ax3, ax4) = axs
-		fig.suptitle('Pressure - Density \n Vz - Vx')
-		ax1.imshow(self.pres,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
-		ax2.imshow(self.Um , extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
-		ax3.imshow(self.vz,  extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
-		ax4.imshow(self.vx,  extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+		fig.suptitle('Pressure - Density \n Vx - Vz')
+		im1 = ax1.imshow(self.pres,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+		im2 = ax2.imshow(self.Um 	,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+		im3 = ax3.imshow(self.vx 	,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+		im4 = ax4.imshow(self.vz 	,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+
+		fig.colorbar(im1, ax=ax1)
+		fig.colorbar(im2, ax=ax2)
+		fig.colorbar(im3, ax=ax3)
+		fig.colorbar(im4, ax=ax4)
 
 		for ax in axs.flat:
 		    ax.label_outer()
 
 		if save: plt.savefig('figures/initial_conditions.png')
 
-		plt.show()
+		# plt.show()
 		plt.close()
 
 	def save_ic(self):

PYTHON/representation.py
@@ -1,14 +1,36 @@
+import numpy as np
 import matplotlib.pyplot as plt 
 
-def drawing_2d(param, Um,pres,vx,vz, time, itt, dt):
+def drawing_2d(init_cond, param, Um,pres,vx,vz, time, itt, dt):
+
+	# computing the curves to draw normalized perturbed ones
+	umnorm = (Um-param.Um00)/param.Um00 
+	pnorm  = (pres-param.p00)/param.p00
+	vxnorm  = vx/init_cond.cs00
+	vznorm  = vz/init_cond.cs00
+
+	# the amplituds must be equal except for a gamma factor 
+	# so we compute one initial amplitud to use it as a frame
+	ampini = np.max(np.abs((init_cond.Um - param.Um00)/param.Um00))
+	# the initial velocity makes the y axes in the velocity to move
+	# towards another equilibrium position [v0-amp,v0+amp] so we compute them
+	v_ampini = init_cond.vx/init_cond.cs00
+	# med   = (max(v_ampini) + min(v_ampini))/2
+	# range =  max(v_ampini) - min(v_ampini)
+    
 
 	fig, axs = plt.subplots(2, 2, sharex='col', sharey='row')
 	(ax1, ax2), (ax3, ax4) = axs
-	fig.suptitle('Pressure - Density \n Vz - Vx')
-	ax1.imshow(pres,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
-	ax2.imshow(Um , extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
-	ax3.imshow(vz,  extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
-	ax4.imshow(vx,  extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+	fig.suptitle('Pressure - Density \n Vx - Vz')
+	im1 = ax1.imshow(pnorm,extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+	im2 = ax2.imshow(umnorm , extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+	im3 = ax3.imshow(vxnorm,  extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+	im4 = ax4.imshow(vznorm,  extent=[param.x0,param.xf,param.z0,param.zf], aspect = 'equal')
+
+	fig.colorbar(im1, ax=ax1)
+	fig.colorbar(im2, ax=ax2)
+	fig.colorbar(im3, ax=ax3)
+	fig.colorbar(im4, ax=ax4)
 
 	for ax in axs.flat:
 	    ax.label_outer()

PYTHON/timestep.py
@@ -16,9 +16,8 @@ import numpy as np
 def  tstep(Um,Upx,Upz,cs, param, grid):
 
 	#compute the characteristic velocities
-	vcharac1 = np.abs(np.sqrt(Upx*Upx + Upz*Upz)/Um + cs)
-	vcharac2 = np.abs(np.sqrt(Upx*Upx + Upz*Upz)/Um - cs)
-
+	vcharac1 = np.abs(np.sqrt(np.square(Upx) + np.square(Upz))/Um + cs)
+	vcharac2 = np.abs(np.sqrt(np.square(Upx) + np.square(Upz))/Um - cs)
 	#compute the proper delta t with the cflparam
 	dt = param.cflparam * np.min([grid.dx,grid.dz]) / np.max([vcharac1, vcharac2])
 